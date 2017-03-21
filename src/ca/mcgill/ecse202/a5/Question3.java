bbac400246da3bfd25711ce000afd4111c66a386
JacobMcConnell <jacob.mcconnell@mail.mcgill.ca>
2017-03-21 10:07:58 -0400

    re add question 3

---
+++ src/ca/mcgill/ecse202/a5/Question3.java
@@ -0,0 +1,32 @@
+package ca.mcgill.ecse202.a5;
+import java.util.Scanner;
+
+public class Question3 {
+
+
+
+  public static void main(String[] args) {
+    int[] array = new int[100];
+    for (int i = 0; i < array.length; i++) {
+      double y = (Math.random() * 100000);
+      array[i] = (int) (y);
+    }
+
+    boolean indexNotPrinted = true;
+    while (indexNotPrinted) {
+      try {
+        System.out.println("Enter and index of the list:");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        System.out.println(array[x] + " is the int at index " + x);
+        input.close();
+        indexNotPrinted = false;
+      }
+        catch (Exception IndexOutOfBoundsException) {
+        System.out.println("Out of Bounds");
+      }
+    }
+  }
+}
+
+
