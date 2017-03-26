ed9e0aab17c497ac5982c6812ed8e1bf29f4de75
JacobMcConnell <jacob.mcconnell@mail.mcgill.ca>
2017-03-26 16:40:20 -0400

    add a few comments

--- src/ca/mcgill/ecse202/a5/Question1.java
+++ src/ca/mcgill/ecse202/a5/Question1.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse202.a5;
 public class Question1{
 public static void main (String[] args){
   MyRectangle2D r1= new MyRectangle2D(2,2,5.5,4.9);
+  //The following code tests the my rectangle class by using its methods on the MyRectangle2D object r1
   System.out.println(r1.getArea());
   System.out.println(r1.getPerimeter());
   System.out.println(r1.contains(3,3));
